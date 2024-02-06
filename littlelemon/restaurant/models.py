4e7c8c1bcb671b637c0fe297e763e12722e7e5a4
HamzaSajjad141 <hamzasajjad1277@gmail.com>
2024-02-06 05:09:37 +0500

    Second-Commit
    In this i made two models named as booking and menu in app models.py file and then apply makemigrations and migrate command in terminal

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+#Create two models here
+
+class Booking(models.Model):
+   first_name = models.CharField(max_length=200)    
+   last_name = models.CharField(max_length=200)
+   guest_number = models.IntegerField()
+   comment = models.CharField(max_length=1000)
+    #define the dunder method
+   def __str__(self):
+      return self.first_name + ' ' + self.last_name
+
+
+
+class Menu (models.Model):
+   name = models.CharField(max_length=200)
+   price = models.IntegerField()
+
+   def __str__(self):
+      return self.name
+
+
